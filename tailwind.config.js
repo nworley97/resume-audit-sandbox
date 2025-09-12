e5a09e39170a99049da119c40b70395fdcd97a20
resumeaudit-admin <nicholasjames.worley@gmail.com>
2025-09-12 10:14:03 -0400

    Updating Sandbox Pass 1

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./static/**/*.js"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        slate: "#475569",
+        "title-black": "#0F172A",
+        "blue-600": "#155CFA",
+        background: "#F9FBFD",
+        primary: "#08C5FF",
+        title: "#1A1C1F",
+        success: "#27AE60",
+        warning: "#F2994A",
+        error: "#EB5757",
+        "apple-blue": "#007AFF",
+      },
+    },
+  },
+  plugins: [],
+}
