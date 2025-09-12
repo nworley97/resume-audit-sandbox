98d3525ccffef2d08fdac4d3d0c3c7a9fe4c7558
resumeaudit-admin <nicholasjames.worley@gmail.com>
2025-09-12 11:00:42 -0400

    Fixing centering

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,20 @@
 module.exports = {
   content: [
     "./templates/**/*.html",
-    "./**/*.html",           // catch-all for templates
-    "./static/**/*.js",
-    "./app.py",              // include Python files in case classes are inline
+    "./static/js/**/*.js",
   ],
   theme: {
     extend: {
       colors: {
-        slate: "#475569",
-        "title-black": "#0F172A",
-        "blue-600": "#155CFA",
+        primary: "#085CFF",
         background: "#F9FBFD",
-        primary: "#08C5FF",
+        slate: "#475569",
         title: "#1A1C1F",
         success: "#27AE60",
         warning: "#F2994A",
         error: "#EB5757",
-        "apple-blue": "#007AFF",
       },
     },
   },
   plugins: [],
-}
+};
